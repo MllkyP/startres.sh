060c2f23be6f80fc8aa36d245608ef22460a3209
MllkyP <90300124+MllkyP@users.noreply.github.com>
2023-10-21 17:01:23 -0700

    Update startres.sh

--- startres.sh
+++ startres.sh
@@ -2,6 +2,6 @@
 #gtf 1920 1080 144
 #xrandr -q to see display port
 
-xrandr --newmode gtf "command output" 
+xrandr --newmode "command output" 
 xrandr --addmode HDMI-1 "1920x1080_144.00"
 xrandr --output HDMI-1 --primary --mode "1920x1080_144.00" --pos 0x0 --rotate normal
